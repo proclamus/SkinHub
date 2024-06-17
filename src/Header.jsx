906f6d19febd041a6f0c8bb7df6bcb4d61d613dd
Proclamus <patrick.yasmim@gmail.com>
2024-06-17 19:39:52 -0300

    tests

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+// Header.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav id="myNavBar" class="navbar">
+      <div class="logoNavBar">
+        <a>
+          <img
+            src={require("./Assets/Logo/Logo_NoBackground_Black.png")}
+            alt="Logo"
+          ></img>
+        </a>
+      </div>
+      <ul id="optionsNavBar" class="optionsNavBar">
+        <li>
+          <Link class="buttonNavBar" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link class="buttonNavBar" to="/About">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link class="buttonNavBar" to="/Contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
